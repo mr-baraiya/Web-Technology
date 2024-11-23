1c7f48ad199b0544cd25acf1279f3fde980ff685
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-11-23 11:26:11 +0530

    Initial commit

---
+++ JAVA SCRIPT/Demo.js
@@ -0,0 +1,5 @@
+let str = 'vishal';
+console.log('str.charAt(4) : ',str.charAt(4));
+console.log('str.substring(1,3) : ',str.substring(1,3));
+console.log('str.substr(1,3) : ',str.substr(1,3));
+console.log(str.replace())
